fa2c75019e2b75b8c9fcb4a046323fbf5e95b8da
Code Sandbox <hello@codesandbox.io>
2023-10-19 04:34:41 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.4f11991449a9b0537d7f6ee6eeece6da.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86838dd274f913899e37ed55211e6c7f",
+    "url": "https://cdrcknt.github.io/csb-wh5nl4/index.html"
+  },
+  {
+    "revision": "16e294f10581a021b43d",
+    "url": "https://cdrcknt.github.io/csb-wh5nl4/static/js/main.f660448d.chunk.js"
+  },
+  {
+    "revision": "4a1bd6c14faf55550ebc",
+    "url": "https://cdrcknt.github.io/csb-wh5nl4/static/js/runtime~main.20784d12.js"
+  }
+]);
